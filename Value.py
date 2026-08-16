f40d5e1b10f434fe05027c45fcd4cc7d92586b0c
ukemichael09-ship-it <ukemichael09@gmail.com>
2026-08-16 19:39:02 +0100

    Add files via upload

---
+++ Value.py
@@ -0,0 +1,13 @@
+# Creating differnet variables
+x= 5
+y = "Manuel" 
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+# User Input
+name = input("enter your name: ")
+
+print("\nHello", name, "\nwelocme to codingal")
