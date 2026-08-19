d012ef545dff1039fec3eb913c53828ef2e51b4a
ukemichael09-ship-it <ukemichael09@gmail.com>
2026-08-19 18:03:29 +0100

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,17 @@
+# Personal Goals Display
+
+# Import the keyword module
+import keyword
+
+# Create variables using valid Python identifier names
+person_name = input("Enter your name: ")
+goal_name = input("Enter your personal goal: ")
+target_month = input("Enter your target month: ")
+daily_minutes = 30
+
+# Print multiple values together
+print("\nName:", person_name)
+print("Goal", goal_name)
+print("Target Month:", target_month)
+print("Daily Practice:", daily_minutes, "minutes")
+
