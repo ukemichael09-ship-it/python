29028dc627ffdb43f2e67c9436c864d8d33e6735
ukemichael09-ship-it <ukemichael09@gmail.com>
2026-08-25 11:59:37 +0100

    Add files via upload

---
+++ home.py
@@ -0,0 +1,20 @@
+# PART 1: Ask the club member for their details
+name = input("Enter your real name, Club Member: ")
+club = input("Enter your school club name: ")
+
+# PART 2: Store the member's details using different data types
+member_number = 8
+points_earned = 9.5
+event_count = 6
+meeting_hours = 1.5
+is_active = True
+
+# PART 3: Print each detail along with itsdata type
+print("Name:", name, "-> type", type (name))
+print("Club:" , club, "-> type", type (club))
+print("Member Number:", member_number, "-> type", type (member_number))
+print("Points Earned:", points_earned, "-> type", type (points_earned))
+print("Event Count :", event_count, "-> type", type (event_count))
+print("Meeting Hours:", meeting_hours, "-> type", type (meeting_hours))
+print("Is Active:", is_active, "-> type", type (is_active))
+
