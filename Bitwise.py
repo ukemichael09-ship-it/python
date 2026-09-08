7b23d0ff2215c15ce8ebb8d24fab4a9957cda322
ukemichael09-ship-it <ukemichael09@gmail.com>
2026-09-08 17:52:59 +0100

    Add files via upload

---
+++ Bitwise.py
@@ -0,0 +1,21 @@
+a = 10
+
+b = -10
+
+
+# print biwise right shift operator
+
+print("a >> 1 =", a >> 1)
+
+print("b >> 1 =", b >> 1)
+
+
+a = 5
+
+b = -10
+
+# print bitwise leftshift operator
+
+print("a << 1 =", a << 1)
+
+print("b << 1 =", b << 1)
