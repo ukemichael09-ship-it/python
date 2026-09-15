e51e23627cde563ab464603db689a787698e7d29
ukemichael09-ship-it <ukemichael09@gmail.com>
2026-09-15 17:57:25 +0100

    Add files via upload

---
+++ ride.py
@@ -0,0 +1,44 @@
+# Custom Ride Builder
+# File  : ride_builder . py
+#Lesson: SPCM2L1 - Nested Statements (Jr)
+
+print("======================================")
+print("     Welcome to Ride Builder!    ")
+print("=======================================")
+print()
+
+print(" Step 1:  Pick your vehicle")
+print("  1 - Bike")
+print(" 2- Car")
+
+choice = int(input(" Enter 1 or 2: "))
+print()
+
+if choice == 1:
+    # Nested is-else - runs only when choiceis 1
+    print("Step 2: Pick your bike type")
+    print("  1 - Scooty")
+    print("  2 - Mountain Bike")
+    print()
+
+    if bike_type == 1:
+        print("You picked  : Scooty")
+        print("Top speed  : 80 km/h")
+        print("Best for  : City roads")
+    else:
+        print("You picked  : Mountain Bike")
+        print("Top speed  : 40 km/h")
+        print("Best for   : Off-road trails")
+
+elif choice == 2:
+    # Nested if-else - runs only when choice is 2
+    print("Step 2: Pick your car type")
+    print("  1 - Sedan")
+    print("  2 - SUV")
+    print()
+
+   if car_type == 1:
+
+
+
+
